7a5fff5fa4e4180db6fcc41f0c02e6173d7b0b1f
William Luna <wluna98@gmail.com>
2023-08-17 02:07:16 -0700

    bill service eslint fix

--- frontend/src/services/bills.js
+++ frontend/src/services/bills.js
@@ -7,6 +7,8 @@ const setToken = newToken => {
   token = `Bearer ${newToken}`
 }
 
+const getToken = () => { return token }
 //const getBill = async obj => {} //access saved bill
 
-export default { setToken }
+// eslint-disable-next-line import/no-anonymous-default-export
+export default { setToken, getToken }
